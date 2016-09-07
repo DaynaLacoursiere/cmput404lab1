009a6d48ead8e8d4aca6ae619bb0b96fbc230455
DaynaLacoursiere <dlacours@ualberta.ca>
2016-09-07 15:10:16 -0600

    new version of python file

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/dlacours')
-print response.status_code
+response = requests.get('https://raw.githubusercontent.com/DaynaLacoursiere/cmput404lab1/master/lab1.py')
+print response.text
